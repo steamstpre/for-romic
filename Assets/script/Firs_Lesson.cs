46bb1c593db74fb47871cbbe1a7654fa74c51cef
steamstpre <denialwins6@gmail.come>
2017-09-20 20:54:01 +0300

    For Romka

---
+++ Assets/script/Firs_Lesson.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firs_Lesson : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.SetFloat("Rist", 170.5f);
+
+        Debug.Log(PlayerPrefs.GetFloat("Comone", -90.1f));
+
+        PlayerPrefs.SetInt("Age", 14);
+
+        Debug.Log(PlayerPrefs.GetInt("Age", 14));
+
+        PlayerPrefs.SetString("Name", "Denial");
+
+        Debug.Log(PlayerPrefs.GetString("Name", "Denial"));
+    }
+	
+	
+		
+	
+}
